b910fc6e2ad563642b0e2c7d0164e471f1e5c417
CakeUmbrellas <tora.batol@gmail.com>
2026-01-09 15:13:24 +0100

    Pusha ny vers av chk-pass.py

--- Projektarbete/chk-pass.py
+++ Projektarbete/chk-pass.py
@@ -2,6 +2,7 @@
 
 import requests
 import hashlib
+import os
 
 # Läs filen med lösenord
 with open('passwords.txt', 'r') as f:
@@ -16,6 +17,19 @@ with open('passwords.txt', 'r') as f:
 		# Gör en förfrågan till "HaveIBeenPwnd" API för att se om lösenordet har läckts
 		response = requests.get(f"https://api.pwnedpasswords.com/range/{password_hash[:5]}")
 
+	#FEATURE1 = Kolla om lösenorden finns i rockyou.txt
+		is_in_rockyou = False
+		if os.path.exists('/usr/share/wordslists/rockyou.txt.gz'):
+			with open('/usr/share/wordslists/rockyou.txt.gz', 'r', encoding='latin-1') as rock_file:
+				for rock_line in rock_file:
+					if rock_line.strip() == password:
+						is_in_rockyou = True
+						break
+		if is_in_rockyou:
+			print(f"Lösenordet för {username} hittades hos rockyou.txt.")
+
+
+
 		# Om responsstatuskoden är 200 betyder det att lösenordet har läckts
 		if response.status_code == 200:
 			# Få listan med hashes från läckta lösenord som börjar med samma 5 symboler som angivet lösenord
@@ -28,3 +42,4 @@ with open('passwords.txt', 'r') as f:
 					break
 			else:
 				print (f"Kunde inte kolla lösenordet för {username}.")
+
